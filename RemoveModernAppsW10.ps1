a195552ef39d4837c79132a83580dbc494131b90
Egger Steve <steve.egger@mtf-be.ch>
2019-06-12 14:04:15 +0200

    Create RemoveModernAppsW10.ps1

---
+++ RemoveModernAppsW10.ps1
@@ -0,0 +1,34 @@
+﻿[string[]]$GameApps = Get-AppxPackage -Name king.com.CandyCrushFriends
+[string[]]$GameApps += Get-AppxPackage -Name king.com.CandyCrushSaga 
+[string[]]$GameApps += Get-AppxPackage -Name Microsoft.XboxApp
+
+
+[string[]]$OtherApps = Get-AppxPackage -Name XINGAG.XING
+[string[]]$OtherApps += Get-AppxPackage -Name Fitbit.FitbitCoach
+[string[]]$OtherApps += Get-AppxPackage -Name Microsoft.NetworkSpeedTest
+[string[]]$OtherApps += Get-AppxPackage -Name Duolingo
+[string[]]$OtherApps += Get-AppxPackage -Name AdobeSystemIncorporated.AdobePhotoshop
+
+
+[string[]]$MSApps = Get-AppxPackage -Name Microsoft.BingNews
+[string[]]$MSApps += Get-AppxPackage -Name Microsoft.BingWeather
+[string[]]$MSApps += Get-AppxPackage -Name Microsoft.GetHelp
+[string[]]$MSApps += Get-AppxPackage -Name Microsoft.Getstarted
+[string[]]$MSApps += Get-AppxPackage -Name Microsoft.Messaging
+[string[]]$MSApps += Get-AppxPackage -Name Microsoft.MicrosoftOfficeHub
+[string[]]$MSApps += Get-AppxPackage -Name Microsoft.People
+[string[]]$MSApps += Get-AppxPackage -Name Microsoft.Wallet
+[string[]]$MSApps += Get-AppxPackage -Name Microsoft.WindowsFeedbackHub
+[string[]]$MSApps += Get-AppxPackage -Name Microsoft.WindowsMaps
+[string[]]$MSApps += Get-AppxPackage -Name Microsoft.YourPhone
+[string[]]$MSApps += Get-AppxPackage -Name Microsoft.Office.Sway
+
+
+[string[]]$RemovingApps = [string[]]$GameApps
+[string[]]$RemovingApps += [string[]]$OtherApps
+[string[]]$RemovingApps += [string[]]$MSApps
+
+foreach($RemovingApp in $RemovingApps){
+    Remove-AppxPackage -Package $RemovingApp
+    Write-Host "Removed App $RemovingApp"
+}
